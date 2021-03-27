e1e46b570ed89e42c83d0e0a82c93334467d444e
juwitaw <juwita.wina@yahoo.com>
2021-03-27 18:15:38 +0700

    create build for article

---
+++ build/precache-manifest.0af80af6526e3003234f92340fe46e5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "042762a8e00f129533a1536672edc70d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab62fcb3aae5a3ff6824",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1f8d6fe7e0ab8437fc59",
+    "url": "/static/js/2.236850f2.chunk.js"
+  },
+  {
+    "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
+    "url": "/static/js/2.236850f2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab62fcb3aae5a3ff6824",
+    "url": "/static/js/main.baeb8d91.chunk.js"
+  },
+  {
+    "revision": "ab1607dc5803dafc7dc5",
+    "url": "/static/js/runtime-main.79d37934.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
